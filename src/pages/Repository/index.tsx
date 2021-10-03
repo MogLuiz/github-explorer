fe7118136b2bec891abf5dfdefdc3ef3dd436c28
Luiz Henrique <luizhjramos@outlook.com>
2021-10-03 19:56:26 -0300

    feat: Header repositorys page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,29 @@
 // Packages
 import React from "react";
-import { useRouteMatch } from "react-router-dom";
+import { FiChevronLeft } from "react-icons/fi";
+import { useRouteMatch, Link } from "react-router-dom";
+
+// Assets
+import logoImg from "../../assets/logo.svg";
 
 // Types
 import { IRepositoryParams } from "./types";
 
+// Styles
+import { Header } from "./styles";
+
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<IRepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <Header>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Link to="/dashboard">
+        <FiChevronLeft size={16} />
+        Voltar
+      </Link>
+    </Header>
+  );
 };
 
 export default Repository;
